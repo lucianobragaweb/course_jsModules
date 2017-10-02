3004994bc89fef2d515f07b943eb57d3c8300a93
lucianobragaweb <lucianobragagroairas@gmail.com>
2017-10-02 00:08:17 -0300

    Lesson 11 - export as

--- src/helpers/arrays.js
+++ src/helpers/arrays.js
@@ -2,7 +2,7 @@ export const map = (arr, fn) => arr.map(fn);
 export const reduce = (arr, init, fn) => arr.reduce(fn, init);
 export const filter = (arr, fn) => arr.reduce(fn);
 
-export function xpto(){}
+export function abc(){}
 
 export class Xpto{
 	constructor(){
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { map, reduce, filter } from './helpers/arrays'
+import { default as arrays, Xpto, abc } from './helpers/arrays'
 
-const a = map([1,2,3,4], x => x * x);
-const b = filter([1,2,3,4], x => x > 3);
+const a = arrays.map([1,2,3,4], x => x * x);
+const b = arrays.filter([1,2,3,4], x => x > 3);
 
+new Xpto()
 console.log({ a, b })
